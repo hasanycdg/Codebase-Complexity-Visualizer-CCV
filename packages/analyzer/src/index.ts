081278a6289fdf3728d951784681b2c146ce744e
florianmattthiashasan <hasan@florianmatthias.com>
2026-02-27 14:30:34 +0100

    index file

---
+++ packages/analyzer/src/index.ts
@@ -0,0 +1,6 @@
+export { analyzeRepository, type AnalyzeRepositoryOptions } from "./core/analyzer.js";
+export { countLoc } from "./core/loc.js";
+export { approximateCyclomaticComplexity } from "./core/complexity.js";
+export { parseJsTsImports } from "./core/importParser.js";
+export { resolveRelativeImport } from "./core/resolver.js";
+export { tarjanScc } from "./core/tarjan.js";
