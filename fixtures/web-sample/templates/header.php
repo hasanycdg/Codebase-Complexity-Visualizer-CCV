013cc52764ad78a48a22d027547ef0776c4d067e
florianmattthiashasan <hasan@florianmatthias.com>
2026-02-27 14:30:53 +0100

    header php

---
+++ fixtures/web-sample/templates/header.php
@@ -0,0 +1,11 @@
+<?php
+include '../styles/reset.css';
+?>
+<!doctype html>
+<html>
+<head>
+  <link rel="stylesheet" href="../styles/main.css" />
+  <script src="../public/app.js"></script>
+</head>
+<body></body>
+</html>
